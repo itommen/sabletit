04a7c2e446b454c95b8cbfa52e9a86513dc742be
Noy <noychn1995@gmail.com>
2018-07-25 16:01:53 +0300

    fixed database again

--- SabletId/app/src/main/java/com/example/ndt/sabletid/Models/AppDatabase.java
+++ SabletId/app/src/main/java/com/example/ndt/sabletid/Models/AppDatabase.java
@@ -1,10 +1,28 @@
 package com.example.ndt.sabletid.Models;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 @Database(entities = {User.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract UserDao userDao();
-}
 
+    private static AppDatabase instance;
+
+    public abstract UserDao userDao();
+
+    private static final Object sLock = new Object();
+
+    public static AppDatabase getInstance(Context context) {
+        synchronized (sLock) {
+            if (instance == null) {
+                instance = Room.databaseBuilder(context.getApplicationContext(),
+                        AppDatabase.class, "subletit-database.db")
+                        .fallbackToDestructiveMigration()
+                        .build();
+            }
+            return instance;
+        }
+    }
+}
