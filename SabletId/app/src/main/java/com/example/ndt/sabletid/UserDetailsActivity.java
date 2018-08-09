ba85ac8f8c58b353b154221f73e14fe3e275aede
Noy <noychn1995@gmail.com>
2018-08-09 21:12:36 +0300

    added delete user button

--- SabletId/app/src/main/java/com/example/ndt/sabletid/UserDetailsActivity.java
+++ SabletId/app/src/main/java/com/example/ndt/sabletid/UserDetailsActivity.java
@@ -1,11 +1,18 @@
 package com.example.ndt.sabletid;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatRadioButton;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ndt.sabletid.Models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -27,7 +34,7 @@ public class UserDetailsActivity extends AppCompatActivity {
         final AppCompatRadioButton rbMale = (AppCompatRadioButton)findViewById(R.id.radioRegisterMale);
         final AppCompatRadioButton rbFemale = (AppCompatRadioButton)findViewById(R.id.radioRegisterFemale);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             FirebaseDatabase.getInstance().getReference("Users")
                     .child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -51,5 +58,41 @@ public class UserDetailsActivity extends AppCompatActivity {
                 }
             });
         }
+
+        findViewById(R.id.btnDeleteUser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseDatabase.getInstance().getReference("Users")
+                                            .child(user.getUid()).removeValue()
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Toast.makeText(UserDetailsActivity.this, "Your account has been deleted successfully",
+                                                                    Toast.LENGTH_LONG).show();
+
+                                                            Intent goToNextActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                                                            startActivity(goToNextActivity);
+                                                        }
+                                                        else {
+                                                            Toast.makeText(UserDetailsActivity.this, "Failed to delete user, please try again",
+                                                                    Toast.LENGTH_LONG).show();
+                                                        }
+                                                    }
+                                                });
+                                }
+                                else {
+                                    Toast.makeText(UserDetailsActivity.this, "Failed to delete user, please try again",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
