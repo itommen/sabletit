bb70119786b32124b232fc3bcc0f9541d7cb8cca
itommen <itommen@gmail.com>
2018-08-31 01:44:10 +0300

    added toolbar & open button

--- SabletId/app/src/main/java/com/example/ndt/sabletid/MasterDetailActivity.java
+++ SabletId/app/src/main/java/com/example/ndt/sabletid/MasterDetailActivity.java
@@ -1,9 +1,15 @@
 package com.example.ndt.sabletid;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MasterDetailActivity extends AppCompatActivity {
 
@@ -11,6 +17,12 @@ public class MasterDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
         // TODO: Check if logged, then move to main
         Fragment newFragment = new LoginActivity();
@@ -20,5 +32,32 @@ public class MasterDetailActivity extends AppCompatActivity {
         transaction.addToBackStack(null);
 
         transaction.commit();
+
+        final NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        ((DrawerLayout)findViewById(R.id.drawer_layout)).closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                ((DrawerLayout)findViewById(R.id.drawer_layout)).openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
