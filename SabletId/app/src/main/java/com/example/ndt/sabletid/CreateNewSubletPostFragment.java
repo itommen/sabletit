52bf801175c33b19d6dedf49ea95edeb9471d464
Noy <noychn1995@gmail.com>
2018-08-31 13:51:36 +0300

    removed redundant code from manifast

--- SabletId/app/src/main/java/com/example/ndt/sabletid/CreateNewSubletPostFragment.java
+++ SabletId/app/src/main/java/com/example/ndt/sabletid/CreateNewSubletPostFragment.java
@@ -1,11 +1,11 @@
 package com.example.ndt.sabletid;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,8 +24,6 @@ public class CreateNewSubletPostFragment extends Fragment {
     private Bitmap imageBitmap;
 
     ImageView ivImage;
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
